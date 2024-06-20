e17109c766eb1145126c7f6574d712eb6c7812b2
shenzh <shen341@gmail.com>
2024-06-21 07:46:43 +0900

    _worker.js2

--- src/_worker.js
+++ src/_worker.js
@@ -1,4 +1,13 @@
-// @ts-ignore
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export default {
+    // @ts-ignore
+    async fetch(request, env) {
+      const url = new URL(request.url);
+      if (url.pathname.startsWith('/api/')) {
+        // TODO: Add your custom /api/* logic here.
+        return new Response('Ok');
+      }
+      // Otherwise, serve the static assets.
+      // Without this, the Worker will error and no assets will be served.
+      return env.ASSETS.fetch(request);
+    },
   }
