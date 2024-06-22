464ae29078e22a14826f2cd837b4d83b72ce6a84
shenzh <shen341@gmail.com>
2024-06-22 23:00:51 +0900

    test

--- src/routes/random/+server.js
+++ src/routes/random/+server.js
@@ -1,5 +1,9 @@
 import { MAILJET_API_KEY,MAILJET_SECRET_KEY,RECEIVER_EMAIL} from "$env/static/private";
 
 export const GET = (() => {
-    return new Response(String(Math.random()));
+    // return new Response(String(Math.random()));
+// JSON.stringify(data)
+
+return new Response(JSON.stringify({"MAILJET_API_KEY":MAILJET_API_KEY}));
+
   })
