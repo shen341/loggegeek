f4cad2101138c34bb996d8b38bc822b51cd0ad08
RyoshunCho <ryoshun.cho@gmail.com>
2024-06-23 21:06:20 +0900

    Footer微修正

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -13,14 +13,18 @@
         <div class="row gy-4">
           <div class="col-lg-4 col-md-6 footer-about">
             <a href="/" class="logo d-flex align-items-center">
+              <!--
               <span class="sitename">株式会社LodgeGeek</span>
+              -->
+              <span class="sitename">{$trans("footer.section.companyName")}</span>
             </a>
             <div class="footer-contact pt-3">
               <p>{$trans("footer.section.address1")}
               </p>
               <p>{$trans("footer.section.address2")}</p>
-              <p><strong>Phone:</strong> <span><a href="tel:03-6824-7905">03-6824-7905</a></span></p>
-              <p><strong>Fax:</strong> <span> 03-4530-8790</span></p>
+              <p><strong>Phone(Japan Local):</strong> <span><a href="tel:03-6824-7905">03-6824-7905</a></span></p>
+              <p><strong>Phone(International):</strong> <span><a href="tel:+81368247905">+81-368247905</a></span></p>
+              <p><strong>Fax(Japan Local):</strong> <span> 03-4530-8790</span></p>
               <p><strong>Email:</strong> <span><a href="mailTo:contact@lodgegeek.com" class="__cf_email__" >contact@lodgegeek.com
               </a></span></p>
             </div>
@@ -33,7 +37,7 @@
           </div>
 
           <div class="col-lg-2 col-md-3 footer-links">
-            <h4>リンク</h4>
+            <h4>Link</h4>
             <ul>
               <li><i class="bi bi-chevron-right"></i> <a href="/#"> {$trans("memu.title.home")}</a></li>
               <li><i class="bi bi-chevron-right"></i> <a href="/#about"> {$trans("memu.title.about")}</a></li>
@@ -60,9 +64,9 @@
 
     <div class="copyright">
       <div class="container text-center">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">LodgeGeek </strong> <span>All Rights Reserved</span></p>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">LodgeGeek Co., Ltd.</strong> <span>All Rights Reserved</span></p>
         <div class="credits">
-          Designed by <a href="/">LodgeGeek</a>
+          Designed by <a href="/">LodgeGeek Co., Ltd.</a> with ❤️️
         </div>
       </div>
     </div>
